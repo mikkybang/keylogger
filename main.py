2ff53c3ab68c6b8cdf4536d79b4072f72341e6b8
Michael Efemena <mefemena@gmail.com>
2020-09-05 02:23:49 +0100

    create on_press function

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
+
+def on_press(key):
+    print("{0} pressed".format(key))
+
+
 with Listener(on_press =on_press, on_release=on_release) as listener:
     listener.join()
